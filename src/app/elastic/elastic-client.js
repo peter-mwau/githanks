a00b33bc0cdeb565774693685a928d95b2679c14
Mickmacha <kimmike1100@gmail.com>
2025-10-02 20:11:42 +0300

    set up elastic cloud credentials

--- src/app/elastic/elastic-client.js
+++ src/app/elastic/elastic-client.js
@@ -4,10 +4,11 @@ import { Client } from '@elastic/elasticsearch';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const elasticApiKey = process.env.ELASTIC_API_KEY;
 const client = new Client({
   node: 'https://cd4c81c879a0450d8f73557509ba34d4.us-central1.gcp.cloud.es.io:443',
   auth: {
-    apiKey: 'cENrV2pKa0JWc1BHNmtSSHI2Rm86SXRpbVBaMVZ0VHBIenNYY0cza2ZsQQ=='
+    apiKey: elasticApiKey,
   },
 });
 
